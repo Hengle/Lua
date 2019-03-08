6c5d3c7343fa63471b3ac7815580b9c1229603aa
tanghaolun <372253837@qq.com>
2019-03-08 23:02:02 +0800

    note

--- 0.lua
+++ 0.lua
@@ -1,111 +1,40 @@
 
-for i=1,30,1 do
-local j = math.random(0,9)
-print (j,1,i)
-end
 
 
+--[[
+a = {}
+a[1]={}
+a[1].Ha = 3
+a[1].Ne = 4
 
------
-local i1 = -1
-
-local i2 = i1 == -1 and false or true
-
-print(i2)
-
-
-a = {0,1,2,3}
-
-
-
-for i,v in pairs(a) do
-
-   if v == 0 then
-   table.remove(a,i)
-   end
-
-end
-
-
-for i,v in pairs(a) do
+for i,v in ipairs(a) do
 
 print(i,v) 
 
 end
 
---[[
-a = 10
+print(a[1].Ha)
+print(a[1].Ne)
 
-b = math.modf(a/3)
-c = math.fmod(a,3)
-
-print(a,b,c)
-
-for i=2,10,2 do
-print (i)
-end
 --]]
 
---[[
+
 a = {}
-a[1] = 22
-a[234] = 3
-a[222] = 4
-a[2] = 8
 
-table.sort(a)
-local siz = #a
-print("--" .. siz .. "--")
-for i,v in ipairs(a) do
- print(i,v)
-end
+a[5] = 3
+a[9] = 10
 
---]]
 
---[[
-function printtb(tb)
+for i,v in pairs(a) do
+local m = v%2
+print (i,v,m)
 
-  local s = ""
-  for i,v in ipairs(tb) do
-     if type(v) == "table" then
-	    s = i .. " --- " 
-	    for m,n in pairs(v) do
-		  s = s .."-key:" .. m .. "-val:" .. n
-		end
-		print(s)
-		s = ""
-	end
-  end
 end
 
 
-function cmp(a,b)
-    if a.e == b.e then
-	   if a.q ==  b.q then
-	     return a.t <b.t
-	   else
-	     return a.q > b.q
-	   end
-	
-	else
-	   return a.e > b.e
-	end
-	 
-end
+local a1 = math.ceil(9.1)
+local a2 = math.ceil(9.0)
+local a3 = math.ceil(9.9)
+local a4 = math.ceil(9.5)
 
-par = {}
-
-par[1] = {e =1 ,q =2, t = 3}
-par[2] = {e =0 ,q=10, t = 1}
-par[3] = {e =0 ,q=4, t = 2}
-par[4] = {e =1 ,q=5, t = 6}
-par[5] = {e =0 ,q=5, t = 6}
-
-print("--original--")
-printtb(par)
-print(par[1].q)
-print("--qulity--")
-table.sort(par, cmp)
-printtb(par)
-
---]]
+print(a1,a2,a3,a4)
