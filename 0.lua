0f60e69546388079ce965662b2f5e9a20b51a734
tanghaolun <372253837@qq.com>
2019-04-02 09:55:40 +0800

    Update 0.lua

--- 0.lua
+++ 0.lua
@@ -1,63 +1,15 @@
 
 
-a = {}
-b = 2.333333
-b_2 = math.ceil(b)
-print(b_2)
+a = false
 
-b_1 = math.floor(b)
-print(b_1)
+b = 1>5
 
+c = 0
 
-print(a.bb)
-print(a["ccc"])
+d = a or b or c
 
---[[
-a = 1e-5
-b = tonumber(a)
-c_1 = math.pi
-c_2 = math.sin(c_1)
-print(c_1 .. "---" .. c_2 )
+print(a or b or c)
 
-p = 9.1
-p_1 = math.ceil(p)
-print("---" .. p_1)
+print(d)
 
---]]
---[[
-a = {}
-a[1]={}
-a[1].Ha = 3
-a[1].Ne = 4
-
-for i,v in ipairs(a) do
-
-print(i,v) 
-
-end
-
-print(a[1].Ha)
-print(a[1].Ne)
-
---]]
-
-
-a = {}
-
-a[5] = 3
-a[9] = 10
-
-
-for i,v in pairs(a) do
-local m = v%2
-print (i,v,m)
-
-end
-
-
-local a1 = math.ceil(9.1)
-local a2 = math.ceil(9.0)
-local a3 = math.ceil(9.9)
-local a4 = math.ceil(9.5)
-
-print(a1,a2,a3,a4)
+print(false)
