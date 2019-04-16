0025887e12d9565b6a6dda68a6344af3720ed002
tanghaolun <372253837@qq.com>
2019-04-16 19:27:31 +0800

    server note

--- 0.lua
+++ 0.lua
@@ -1,4 +1,5 @@
 
+--[[
 require "bit"
 a = 2
 
@@ -8,3 +9,16 @@ b_1 = bit.rshift(a,0)
 c = bit.band(b,b_1)
 
 print(a.."  /  ".. b .."  /  ".. b_1 .."  /  "..  c .."  /  ")
+--]]
+
+c = 10.12345
+local a , b = math.modf(c);
+
+print(a,"---",b)
+
+
+local f = math.pow(10, 2)
+local d = b *f
+local d1,d2 = math.modf(d)
+local r = a .. "." .. d1
+print(d1,"---",d2,"---",r)
