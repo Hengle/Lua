7410bcf420ec439c7fa8edc757005a1c6374f8b0
tanghaolun <372253837@qq.com>
2019-05-21 15:31:02 +0800

    使用转换矩阵移动此坐标上的点来进行差值移动

---
+++ Client_czy_modle_not/两点之间的移动Fly脚本/CustomFly/FlyTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CustomFly))]
+public class FlyTest : MonoBehaviour {
+    public Transform TargetTrans;
+
+    [ContextMenu("Fly")]
+    public void Fly()
+    {
+        CustomFly FlyCom = gameObject.GetComponent<CustomFly>();
+        FlyCom.Fly(TargetTrans.position);
+    }
+}
