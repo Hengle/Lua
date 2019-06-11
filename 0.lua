6412e501aeb62af7b18af25bbac3551d6ec8532b
tanghaolun <372253837@qq.com>
2019-06-11 20:09:12 +0800

    sd

--- 0.lua
+++ 0.lua
@@ -1,33 +1,5 @@
 
 --[[
-require "bit"
-a = 2
-
-b = 1
-b_1 = bit.rshift(a,0)
-
-c = bit.band(b,b_1)
-
-print(a.."  /  ".. b .."  /  ".. b_1 .."  /  "..  c .."  /  ")
---]]
-
---[[
-c = 10.12345
-local a , b = math.modf(c);
-
-print(a,"---",b)
-
-
-
-
-
-local f = math.pow(10, 2)
-local d = b *f
-local d1,d2 = math.modf(d)
-local r = a .. "." .. d1
-print(d1,"---",d2,"---",r)
---]]
---
 local c = math.mod(20,101)
 print(c)
 
@@ -35,3 +7,5 @@ for i=1,100,1 do
  local m = math.random(0,9)
   print(m)
  end
+ 
+ --]]
