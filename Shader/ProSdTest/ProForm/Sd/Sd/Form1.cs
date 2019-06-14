a160be2e2d55fbbac26070e77ecf5c2ef1a84204
tanghaolun <372253837@qq.com>
2019-06-14 19:19:40 +0800

    sd form 17 matrix

---
+++ Shader/ProSdTest/ProForm/Sd/Sd/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sd
+{
+    public partial class Form1 : Form
+    {
+        Triangle t;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PointF A = new PointF(0,-100);
+            PointF B = new PointF(100,100);
+            PointF C = new PointF(-100,100);
+            t = new Triangle(A,B,C);
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(150,110);
+            t.Draw(e.Graphics);
+        }
+    }
+}
---
+++ Shader/ProSdTest/ProForm/Sd/Sd/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Sd
+{
+    class Triangle
+    {
+        PointF A, B, C;
+
+        public Triangle(PointF A, PointF B, PointF C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Red,2);
+            g.DrawLine(pen, A, B);
+            g.DrawLine(pen, B, C);
+            g.DrawLine(pen, C, A);
+        }
+    }
+}
