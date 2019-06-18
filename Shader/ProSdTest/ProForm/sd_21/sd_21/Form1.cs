7077beeaa37185c09ad384d5cec8c1bc545b0a3f
tanghaolun <372253837@qq.com>
2019-06-18 21:04:00 +0800

    sd matrix

---
+++ Shader/ProSdTest/ProForm/sd_21/sd_21/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sd_21
+{
+    public partial class Form1 : Form
+    {
+        int a;
+        Triangle3D t;
+
+        Matrix4x4 m_scale;//缩放
+        Matrix4x4 m_rotation;//旋转
+        Matrix4x4 m_view;//平移
+        Matrix4x4 m_projection;//投影矩阵
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //初始化
+            m_scale = new Matrix4x4();
+            m_scale[1, 1] = 199;
+            m_scale[2, 2] = 199;
+            m_scale[3, 3] = 199;
+            m_scale[4, 4] = 1;
+
+
+            //平移
+            m_view = new Matrix4x4();
+            m_view[1, 1] = 1;
+            m_view[2, 2] = 1;
+            m_view[3, 3] = 1;
+            m_view[4, 3] = 199; //z平移
+            m_view[4, 4] = 1;
+ 
+
+            //旋转
+            m_rotation = new Matrix4x4();
+
+
+            //投影
+            m_projection = new Matrix4x4();
+            m_projection[1, 1] = 1;
+            m_projection[2, 2] = 1;
+            m_projection[3, 3] = 1;
+            m_projection[3, 4] = 1.0 / 199; //投影到小孔的距离
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //只有最后是1是点跟矩阵相乘才有意义
+            Vector4 a = new Vector4(0, -0.5, 0, 1); 
+            Vector4 b = new Vector4(0.5, 0.5, 0, 1);
+            Vector4 c = new Vector4(-0.5, 0.5, 0, 1);
+            t = new Triangle3D(a,b,c);
+
+            //缩放
+            //t.Transform(m_scale);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            t.Draw(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a += 2;
+            
+            //弧度
+            double angle = a / 360.0 * Math.PI; 
+            m_rotation[1, 1] = Math.Cos(angle);
+            m_rotation[1, 3] = Math.Sin(angle);
+            m_rotation[2, 2] = 1;
+            m_rotation[3, 1] = -Math.Sin(angle);
+            m_rotation[3, 3] = Math.Cos(angle);
+            m_rotation[4, 4] = 1;
+
+            //联合矩阵
+            Matrix4x4 m = m_scale.Mul(m_rotation);
+            m = m.Mul(m_view);
+            m = m.Mul(m_projection);
+
+
+            //缩放
+            //t.Transform(m_scale);
+            t.Transform(m);
+            this.Invalidate();
+        }
+    }
+}
---
+++ Shader/ProSdTest/ProForm/sd_21/sd_21/Triangle3D.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sd_21
+{
+    class Triangle3D
+    {
+        private Vector4 a, b, c; //变换后的值
+        public Vector4 A, B, C;
+
+        public Triangle3D() { }
+        public Triangle3D(Vector4 a, Vector4 b, Vector4 c)
+        {
+            //引用的赋值，所以改变了则传参的数据也会被改变
+            /*
+            this.A = a;
+            this.B = b;
+            this.C = c;
+            */
+
+            //不用引用赋值，直接new赋值
+            this.A = new Vector4(a);
+            this.B = new Vector4(b);
+            this.C = new Vector4(c);
+
+            this.a = this.A;
+            this.b = this.B;
+            this.c = this.C;
+        }
+
+        //三角形用矩阵的乘法进行变换
+        public void Transform(Matrix4x4 m)
+        {
+            this.a = m.Mul(this.A);
+            this.b = m.Mul(this.B);
+            this.c = m.Mul(this.C);
+        }
+        
+
+        //绘制三角形到2d窗口上
+        public void Draw(Graphics g)
+        {
+            g.TranslateTransform(150, 150);
+            g.DrawLines(new Pen(Color.Blue,2),this.Get2DPointFArr());
+        }
+
+        private PointF[] Get2DPointFArr()
+        {
+            PointF[] arr = new PointF[4];
+            arr[0] = Get2DPointF(this.a);
+            arr[1] = Get2DPointF(this.b);
+            arr[2] = Get2DPointF(this.c);
+            arr[3] = arr[0];
+            return arr;
+        }
+
+        private PointF Get2DPointF(Vector4 v)
+        {
+            PointF p = new PointF();
+            p.X = (float)(v.x / v.w);
+            p.Y = (float)(v.y / v.w);
+
+            return p;
+        }
+    }
+}
