66e0bcbf5075d234cdaefc92930c37ce5bbefdbe
tanghaolun <372253837@qq.com>
2019-09-10 09:41:06 +0800

    change file name

--- 0.lua
+++ 0.lua
@@ -9,3 +9,38 @@ for i=1,100,1 do
  end
  
  --]]
+
+
+--[[
+ae = {
+    a = 0,
+    b = -1,
+}
+
+c = "w4\3myname04i0i"
+
+local i1 = string.find(c,"myname")
+local i2 = string.find(c,"1myname")
+print(i1)
+print(i2)
+
+d = string.gsub(c,"\\","/")
+d_2 = string.gsub(d,"w4/3","")
+print(d_2)
+--]]
+local a.c = {}
+local a.c.b = 4
+
+print(a.c.b)
+
+
+
+
+
+
+
+
+
+
+
+
